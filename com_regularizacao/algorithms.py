566531bc7d4dd606fb477434bab997ce675c5ca4
Gabriel <ligabogabriel@gmail.com>
2026-01-20 14:40:40 -0300

    Fazendo diversos experimentos e concluindo que não há garantia de convergência ao usar o passo exato

--- com_regularizacao/algorithms.py
+++ com_regularizacao/algorithms.py
@@ -50,8 +50,8 @@ class Algorithms:
             num = np.sum( g_f * g_f )  
             den = np.sum( g_f * Q( g_f ) )
             step = num / den
-            if iterations % 50 == 0:
-                print( f'O tamanho do passo para o ista com passo exato foi de: {step}' )
+            # if iterations % 50 == 0:
+            #     print( f'O tamanho do passo para o ista com passo exato foi de: {step}' )
 
             y = x0 - step * g_f
             x = prox( x = y, step = step, gamma = gamma, wavelet = wavelet )
@@ -141,8 +141,65 @@ class Algorithms:
             num = np.sum( g_y * g_y )  
             den = np.sum( g_y * Q( g_y ) )
             step = num / den
-            if iterations % 50 == 0:
-                print( f'O tamanho do passo para o fista com passo exato foi de: {step}' )
+            # if iterations % 50 == 0:
+            #     print( f'O tamanho do passo para o fista com passo exato foi de: {step}' )
+            ygrad = y0 - step * g_y
+        
+            x = prox( x = ygrad, step = step, gamma = gamma, wavelet = wavelet )
+            t = ( 1 + np.sqrt( 1 + 4*t0 ** 2 ) ) / 2
+            y = x + ( ( t0 - 1 ) / ( t ) )  * ( x - x0 )
+            
+            dif = np.linalg.norm( x - x0 )
+
+
+            y0 = y
+            t0 = t
+            iterations += 1
+            iters.append( x.copy() )
+
+
+        return iters
+
+    @staticmethod
+    def ista_exato_relaxado( f, x0, grad_f, prox, gamma, eta, Q, max_iter = 1000, tol = 1.0e-1, wavelet = 'haar' ):
+        x = x0.copy()
+        iterations = 0
+        iters = [ x.copy() ]
+        dif = float( 'inf' )
+
+        while dif > tol and iterations < max_iter:
+            x0 = x.copy()
+            g_f = grad_f( x0 )
+
+            num = np.sum( g_f * g_f )  
+            den = np.sum( g_f * Q( g_f ) )
+            step = eta * ( num / den )
+
+            y = x0 - step * g_f
+            x = prox( x = y, step = step, gamma = gamma, wavelet = wavelet )
+                    
+            iterations += 1
+            iters.append( x.copy() )
+            dif = np.linalg.norm( x - x0 )
+
+        return iters
+
+
+    @staticmethod
+    def fista_exato_relaxado( f, x0, Q, grad_f, prox, gamma, eta, max_iter = 1000, tol = 1.0e-1, wavelet = 'haar'  ):
+        y0 = x0.copy()
+        x = x0.copy()
+        iterations = 0
+        t0 = 1
+        iters = [ x.copy() ]
+        dif = float( 'inf' )
+
+        while dif > tol and iterations < max_iter:
+            x0 = x.copy()
+            g_y = grad_f( y0 )
+            num = np.sum( g_y * g_y )  
+            den = np.sum( g_y * Q( g_y ) )
+            step = eta * ( num / den )
             ygrad = y0 - step * g_y
         
             x = prox( x = ygrad, step = step, gamma = gamma, wavelet = wavelet )
